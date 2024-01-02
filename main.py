9b48fed1083397c901f183ac18c0ef78d194951c
Rungroj Sonsubanan <pocky@MacBook-Air-khxng-Rungroj.local>
2024-01-02 12:06:45 +0700

    feat: Add button sheetcheat

--- main.py
+++ main.py
@@ -2,7 +2,27 @@ import streamlit as st
 
 st.write('Hello, *World!* :sunglasses:')
 
-values = st.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
+st.header("Sheet Cheat", divider = 'violet')
+st.subheader("Input Element")
+
+
+text_field_code = '''
+st.button("Button")
+st.button("Reset", type="primary")
+'''
+st.code(text_field_code, language='python')
+st.button("Button")
+st.button("Reset", type="primary")
+
+# pattern 
+# sub-header
+# snippet
+# element
+
+
+# input_element = '''If you end a line with two spaces,
+# a soft return is used for the next line.
+
+# Two (or more) newline characters in a row will result in a hard return.'''
+
+# # st.markdown(input_element)
